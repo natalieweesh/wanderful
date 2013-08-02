af3138f195786d9e5b703f50ef62d570e01125db
Natalie Wee <natalieweesh@gmail.com>
2013-08-02 16:49:08 -0400

    First commit. Implement devise.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capstone::Application.config.secret_token = '26c426d2b3e75edd8a8a86a368b74268692e7dd3f5130f52ef5c2c7aa847fb628360ef1557aca180c58bbf2b371e032dec419641540d38378d8f31a8434f6e0e'
