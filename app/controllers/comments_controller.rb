ecca8698e8eb413c85a679333ef7b10d887b0b44
Natalie Wee <natalieweesh@gmail.com>
2013-08-13 17:28:59 -0400

    Make comments deletable.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,5 +20,29 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
   end
   
+  def destroy
+    @comment = Comment.find(params[:id])
+    @activity_id = @comment.activity_id
+    if @comment.child_comments.empty?
+      @comment.destroy
+    else
+      @comment.body = "This comment has been deleted."
+      @comment.save
+    end
+    
+    if request.xhr?
+      p "STUFFF STUFFF STUFFF STUFFF STUFFF STUFFF STUFFF STUFFF STUFFF STUFFF STUFFF STUFFF STUFFF STUFFF"
+      if @comment.child_comments.empty?
+        render json: "Hahaha.".to_json 
+      else
+        render json: "This comment has been deleted.".to_json
+
+      end
+    else
+      redirect_to activity_url(@activity_id)
+    end
+
+  end
+  
   
 end
