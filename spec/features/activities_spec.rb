d713d334817736681491b1766fdef7f5eb665261
Natalie Wee <natalieweesh@gmail.com>
2013-08-15 12:29:30 -0400

    Organize js and css files a bit.

---
+++ spec/features/activities_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+feature "Creating an activity" do
+  
+  context "when logged in" do #, js: true do
+    
+    # before :each do
+    #   sign_up_as_hello_world
+    #   click_button "share-something"
+    #   # click_button "add \n an \n activity"
+    # end
+    
+    it "has a new activity page" do
+      # sign_up_as_hello_world
+
+      # click_link "share-something"
+      # click_button "share-something"
+      # page.should have_content "stuff"
+    end
+    
+  end
+  
+end
